a1d9bb9387b21de0e10bd0a8d1f12a7b93fad746
jhbecares <jhdez.becares@gmail.com>
2015-05-23 15:48:12 +0200

    Minor changes to adams-bashforth method

--- mispracticas.m
+++ mispracticas.m
@@ -1,3 +1,3 @@
  %f=@funcvanderpol; x0 = [0.1; 0.2]; intervalo = [0,10]; N = 1000;
- %f=@funccorazon; x0 = [0; 2]; intervalo = [0,2*pi]; N = 1000;
- f=@funcrigida; x0 = 0; intervalo = [0,pi/2]; N = 50;
+ f=@funccorazon; x0 = [0; 2]; intervalo = [0,2*pi]; N = 1000;
+ %f=@funcrigida; x0 = 0; intervalo = [0,pi/2]; N = 50;
--- testmiab4.m
+++ testmiab4.m
@@ -1,5 +1,3 @@
-mispracticas
-
-[t,u] = miab4(intervalo(1), intervalo(2), N, x0, f, []);
-
-misgraficas 
+mispracticas;
+[t, u] = miab4(intervalo(1), intervalo(2), N, x0, f, []); 
+misgraficas;
