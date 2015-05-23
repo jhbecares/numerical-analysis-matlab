3ad1246b3716b05b656df7fc13da9cac77509a2f
jhbecares <jhdez.becares@gmail.com>
2015-05-23 19:22:52 +0200

    Predictor-corrector with milne (pred) and simpson (corr) added

--- mimilsimp.m
+++ mimilsimp.m
@@ -1,44 +1,51 @@
-%   mimilsimp.m
-%
-%   Funcion que implementa el metodo de prediccion correccion que usa como
-%   Predictor -> Metodo de Milne de 4 pasos
-%   Corrector -> Metodo de Simpson de 2 pasos
-%
-%   Inicializa el metodo utilizando el metodo de Runge-Kutta de orden 4
-
-function [t, u] = mimilsimp(t0, tfin, N, x0, f, param)
-
-    h = (tfin-t0) / N;
-    t = (t0:h:tfin);
-    u = x0;    
-
-
-    % Inicializamos los 4 primeros valores por RK-4
-    intervalo2 = [t(1), t(5)];
-    [~, u2] = mirk4(intervalo2(1), intervalo2(2), 3, x0, f, param);
-    for i = 1 : 4
-        u(:, i) = u2(:, i);
+function [t, u] = mimilsimp (t0, tfin, N, x0, f, par)
+    % ENTRADA
+    % t0 = tiempo inicial
+    % tfin = tiempo final T
+    % N = Numero de pasos
+    % x0 = valor inicial, vector columna
+    
+    % f(t,x) funcion de la EDO
+    % par = variable de entrada con parametros. 
+    
+    % SALIDA
+    % t = vector fila de tiempos t(n)
+    % u = tabla de valores de x(t(n))
+    % numfun = contador del numero de evaluaciones de la funcion
+    
+    
+    h = (tfin - t0)/N; % paso
+    t = (t0:h:tfin); % mallado de puntos
+    u = zeros(length(x0), N+1);
+    u(:,1) = x0; % Incorporamos el dato inicial ; 
+    fun(:,1) = feval(f,t(1),u(:,1));
+    
+    
+    % Inicializaciones
+    [tAux, uAux] = mirk4(t0, t0+3*h, 3, x0, f, par);
+    
+    % Suponemos que tenemos ya desde u(:,1) hasta u(:,4)
+    for i=2:4
+        u(:,i) = uAux(:,i);
+        fun(:,i) = feval(f,t(i),u(:,i));
     end
-
-    % Milne [4 pasos]
-    % u[I] = u[I-4] + 4h/3 * (2 f[I-1] - f[I-2] + 2 f[I-3])
-    % Simpson [2 pasos]
-    % u[I] = u[I-2] + h/3 * (f[I] + 4 f[I-1] + f[I-2])
-
-    k3 = feval(f, t(2), u(:, 2));
-    k2 = feval(f, t(3), u(:, 3));
-
-    for i = 5 : N + 1
-        k1 = feval(f, t(i - 1), u(:, i - 1));
-
-        % Predictor [Milne]
-        uAux = u(:, i-4) + (4*h/3)*(2*k1 - k2 + 2*k3);
-
-        % Corrector [Simpson]
-        fAux = feval(f, t(i), uAux);
-        u(:, i) = u(:, i-2) + (h/3)*(fAux + 4*k1 + k2);
-
-        k3 = k2;
-        k2 = k1;
+    
+    for i = 1:N-3
+       % Metodo predictor (Milne)
+%         u(:,i+1) = u(:,i-3)+ (4*h/3)*(2*fun(:,i) -fun(:,i-1) + 2*fun(:,i-2));
+%         fun(:,i+1) = feval(f,t(i+1),u(:,i+1));
+       
+       z = u(:,i)+ (4*h/3)*(2*fun(:,i+3) -fun(:,i+2) + 2*fun(:,i+1));
+      % z = u(:,i+3)+ (h/24)*(55*fun(:,i+3) -59*fun(:,i+2) + 37*fun(:,i+1) -9*fun(:,i));
+       
+       % Metodo corrector (Simpson)
+       % u[I] = u[I-2] + h/3 * (f[I] + 4 f[I-1] + f[I-2])
+       fun(:,i+4) = feval(f,t(i+4),z);
+       u(:,i+4) = u(:,i+2)+(h/3)*(fun(:,i+4)+4*fun(:,i+3)+fun(:,i+2));
+       %u(:,i+4) = u(:,i+3)+(h/24)*(9*fun(:,i+4)+19*fun(:,i+3)-5*fun(:,i+2)+fun(:,i+1));
+       fun(:,i+4) = feval(f,t(i+4),u(:,i+4));
     end
-end
+   
+ end
+
+ 
--- testmimilsimp.m
+++ testmimilsimp.m
@@ -1,15 +1,3 @@
-%       testmieuler.m
-%
-%       Resolucion de EDIFs utilizando mimilsimp.m:
-%       [Implementacion del metodo de prediccion(Milne)-correccion(Simpson) para la resolucion de EDIFs]
-%
-%       Utiliza:
-%           mispracticas.m
-%           misgraficas.m
-%           mimilsimp.m
-
-
-
-mispracticas;             
-[t, u] = mimilsimp(intervalo(1), intervalo(2), N, x0, f, []);  
-misgraficas;                 
+mispracticas;
+[t, u] = mimilsimp(intervalo(1), intervalo(2), N, x0, f, []); 
+misgraficas;
